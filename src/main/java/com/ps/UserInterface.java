7b3ccef90e67bc5d740d95df296bc8a4c7c2ebdd
Daomei Dong <daomeidong@Macmini.home>
2025-05-27 19:52:40 -0400

    add home screen

--- src/main/java/com/ps/UserInterface.java
+++ src/main/java/com/ps/UserInterface.java
@@ -15,11 +15,47 @@ public class UserInterface {
     }
 
     public void display(){
+        System.out.println("Welcome to the \"DELI-cious\" home screen!" +
+                "\n1) New Order" +
+                "\n0) Exit" +
+                "\nChoose your option: ");
 
+        int choice;
+        boolean running = true;
+
+        while (running){
+            choice = getIntInput();
+            switch (choice){
+                case 1:
+                    processNewOrderRequest();
+                    break;
+                case 0:
+                    System.out.println("Thank you for using our app, see you again! ");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid input, try again. ");
+            }
+        }
+    }
+
+    private void processNewOrderRequest(){
+        System.out.println("Welcome to order screen! ");
     }
 
     private void checkout(){
 
+    }
+
+    // helper method to get user int input and consumes redundant \n
+    private int getIntInput(){
+        while( ! scanner.hasNextInt()){
+            System.out.println("Invalid input, enter an integer: ");
+            scanner.nextLine(); // or scanner.next() ?
+        }
+        int result = scanner.nextInt();
+        scanner.nextLine(); // consumes redundant \n, or scanner.next() ?
+        return result;
     }
 }
 
